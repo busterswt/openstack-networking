ae3c445ad22feb6ae0275153d1b1ab198b051604
James Denton <james.denton@rackspace.com>
2015-01-08 17:09:44 -0600

    Update raxmon.py

--- raxmon.py
+++ raxmon.py
@@ -13,6 +13,7 @@ SLEEP_TIME = 20
 TENANT_NAME = 'admin'
 USERNAME = 'admin'
 PASSWORD = 'Hc3RceSQ'
+
 KEYSTONE_ADMIN_ADDRESS = '172.24.240.93'
 KEYSTONE_ADMIN_PORT = '5000'
 
@@ -28,15 +29,16 @@ def test_keystone_service(raxmon, tenant_name, username, password, address, port
 
     # define test pool
     payload = {"auth": {"tenantName": tenant_name, "passwordCredentials": {"username": username, "password": password}}}
-    #payload['auth']['tenant_name'] = tenant_name
-    #payload['auth']['passwordCredentials']['username'] = username
-    #payload['auth']['passwordCredentials']['password'] = password
 
     RAXMON_URL_BASE = 'http://%s:%s' % (address,port)
-    #raxmon.post('%s/v2.0/tokens' % RAXMON_URL_BASE, data=json.dumps(payload))
     r = raxmon.post('%s/v2.0/tokens' % RAXMON_URL_BASE, data=json.dumps(payload))
     print r.status_code
+    #print json.loads(data)["access"]["token"]["id"]
+    tenantid = json.loads(r.text)["access"]["token"]["id"]
     logging.info('[keystone_admin] %s', r.status_code)
+    return tenantid
     
 # Test keystone service using 'keystone tenant-list'
-test_keystone_service(raxmon, TENANT_NAME, USERNAME, PASSWORD, KEYSTONE_ADMIN_ADDRESS, KEYSTONE_ADMIN_PORT)
+tenantid = test_keystone_service(raxmon, TENANT_NAME, USERNAME, PASSWORD, KEYSTONE_ADMIN_ADDRESS, KEYSTONE_ADMIN_PORT)
+print tenantid
+
